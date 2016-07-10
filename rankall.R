fe7b918ac728db6e87d432e3cda587e041c3b6d2
Brett Crosby <brett@crosby.com.au>
2016-07-10 09:34:41 +0930

    These are the final working copies of the Programming Assignment 3

--- rankall.R
+++ rankall.R
@@ -5,23 +5,24 @@
 setwd("C:/Users/bcrosby/git/Coursera/ProgAss3")
 
 
-best <- function(state, outcome) {
-    # Error Checking - valid outcome
-    if(! outcome %in% c("heart attack", "heart failure", "pneumonia")) stop("invalid outcome")
-
+rankall <- function(outcome, num = "best") {
+    # Error Checking - valid outcome and valid rank
+    if(!outcome %in% c("heart attack", "heart failure", "pneumonia")) stop("invalid outcome")
+    if(!num %in% c("best", "worst") && !is.numeric(num)) stop("invalid ranking")
+    
     # Set up indices for each condition. This matches the column numbers
     # in the data file but means I can refer to the data by a better name.
     Hospital     <- 2
     HeartAttack  <- 11
     HeartFailure <- 17
     Pneumonia    <- 23
-
+    
     # Use a switch statement to choose the correct outcome
     Result <- switch (outcome,
                       "heart attack"  = HeartAttack,
                       "heart failure" = HeartFailure,
                       "pneumonia"     = Pneumonia)
-
+    
     # I want to only read in the columns that I'm going to use
     # Use the indexes defined above to create a column import list
     TotalCols <- 46               # the total number of columns in the data file
@@ -30,11 +31,11 @@ best <- function(state, outcome) {
     ColTypes <- c("NULL",
                   "character",
                   rep("NULL", 4), # pad out between column 2 and 7
-                  "character",
+                  "factor",
                   rep("NULL", SkipCols), # skip the columns between state and outcome
                   NA,
                   rep("NULL", EndCols)) # skip unwanted columns at the end of the file
-
+    
     # Read outcome-of-care-measures.csv
     OutcomeTable <- read.csv("outcome-of-care-measures.csv",
                              header = TRUE,
@@ -44,23 +45,38 @@ best <- function(state, outcome) {
     
     # Rename the columns to make them easier to work with
     colnames(OutcomeTable) <- c("Hospital", "State", "Result")
-
-    # Now that we've read the data, confirm the State is valid
-    if(! state %in% OutcomeTable$State) stop("invalid state")
     
-    # Remove any NA values
-    OutcomeTable <- na.omit(OutcomeTable[OutcomeTable$State == state, ])
-
-    # # Order the results by outcome value and then hospital name
-    OutcomeTable <- OutcomeTable[order(OutcomeTable$Result, OutcomeTable$Hospital), ]
+    # Order the results by outcome Result and then Hospital name
+    # This gives me the data ordered and ready to split by state
+    OutcomeTable <- OutcomeTable[order(OutcomeTable$State, OutcomeTable$Result, OutcomeTable$Hospital), ]
+    
+    # Get rid of any NA values
+    OutcomeTable <- na.omit(OutcomeTable)
+    
+    # Split the data frame by State
+    split_data <- split(OutcomeTable, OutcomeTable$State)   
+    
+    # Use sapply to iterate through the list of state data frames and extract the
+    # Hospital at the appropriate index
+    Hospitals <- sapply(split_data, function(x) {
+        if(num == "best") {
+            xRow <- 1
+        } else if(num == "worst") {
+            xRow <- nrow(x)
+        } else {
+            xRow <-num
+        }
+        x[xRow, "Hospital"]
+    })
+    
+    # Build a dataframe with the Hospital and State data
+    FinalResult <- data.frame(hospital=Hospitals, state=names(Hospitals), row.names=names(Hospitals))
 
     # Return the value
-    OutcomeTable[1, "Hospital"]
+    FinalResult
 }
 
-best("TX", "heart attack")
-best("TX", "heart failure")
-best("MD", "heart attack")
-best("MD", "pneumonia")
-best("BB", "heart attack")
-best("NY", "hert attack")
+head(rankall("heart attack", 20), 10)
+tail(rankall("pneumonia", "worst"), 3)
+tail(rankall("heart failure"), 10)
+
--- rankhospital.R
+++ rankhospital.R
@@ -5,11 +5,11 @@
 setwd("C:/Users/bcrosby/git/Coursera/ProgAss3")
 
 
-best <- function(state, outcome, num = "best") {
+rankhospital <- function(state, outcome, num = "best") {
     # Error Checking - valid outcome and valid rank
-    if(! outcome %in% c("heart attack", "heart failure", "pneumonia")) stop("invalid outcome")
-    # if(!num %in% c("best", "worst") | !is.numeric(num)) stop("invalid ranking")
-
+    if(!outcome %in% c("heart attack", "heart failure", "pneumonia")) stop("invalid outcome")
+    if(!num %in% c("best", "worst") && !is.numeric(num)) stop("invalid ranking")
+    
     # Set up indices for each condition. This matches the column numbers
     # in the data file but means I can refer to the data by a better name.
     Hospital     <- 2
@@ -26,12 +26,12 @@ best <- function(state, outcome, num = "best") {
     # I want to only read in the columns that I'm going to use
     # Use the indexes defined above to create a column import list
     TotalCols <- 46               # the total number of columns in the data file
-    SkipCols <- (Result - 1) - 7  # The # of colums between the state and the outcome
+    SkipCols <- (Result - 1) - 7  # The # of columns between the state and the outcome
     EndCols <- TotalCols - Result # the # of columns after the outcome
     ColTypes <- c("NULL",
                   "character",
                   rep("NULL", 4), # pad out between column 2 and 7
-                  "character",
+                  "factor",
                   rep("NULL", SkipCols), # skip the columns between state and outcome
                   NA,
                   rep("NULL", EndCols)) # skip unwanted columns at the end of the file
@@ -55,16 +55,19 @@ best <- function(state, outcome, num = "best") {
     # # Order the results by outcome Result and then Hospital name
     OutcomeTable <- OutcomeTable[order(OutcomeTable$Result, OutcomeTable$Hospital), ]
     
+    # if the input value was either "best" or "worst" convert that to an index value
     rank <- switch(num,
                    "best" = 1,
                    "worst" = nrow(OutcomeTable))
     
-    if(! is.numeric(rank)) rank <- as.numeric(num)
+    # If rank is not set then use the numeric value passed in
+    if(!is.numeric(rank)) rank <- as.numeric(num)
     
     # Return the value
     OutcomeTable[rank, "Hospital"]
 }
 
-best("TX", "heart failure", 4)
-best("MD", "heart attack", "worst")
-best("MN", "heart attack", 5000)
+rankhospital("TX", "heart failure", 4)
+rankhospital("MD", "heart attack", "worst")
+rankhospital("MD", "heart attack", "best")
+rankhospital("MN", "heart attack", 5000)
